50477b2355148ae73653beedb2bb64b9a0771096
Ram Goli <ramsgoli@gmail.com>
2019-11-01 20:35:14 -0700

    playlist selection page with columns

--- server/routes/public/index.js
+++ server/routes/public/index.js
@@ -1,7 +1,37 @@
 var express = require('express')
 var router = express.Router()
+const { createAuthenticatedSpotifyApi } = require('../../spotify')
 
 class PublicRouter {
+  // checks that access token is present
+  static async middleware(req, res, next) {
+    const spotifyApi = createAuthenticatedSpotifyApi()
+
+    // get access token from cookie
+    if (!req.cookies['access_token']) {
+      // refresh token
+      try {
+        spotifyApi.setRefreshToken(req.cookies['refresh_token'])
+        const data = await spotifyApi.refreshAccessToken()
+
+        res.cookie('access_token', data.body['access_token'], {
+          httpOnly: true,
+          maxAge: data.body['expires_in']
+        })
+
+        spotifyApi.setAccessToken(data.body['access_token'])
+      } catch (err) {
+        // redirect to login route
+        return res.redirect('/api/v1/auth/login')
+      }
+    } else {
+      const access_token = req.cookies['access_token']
+      spotifyApi.setAccessToken(access_token)
+    }
+    req.spotifyApi = spotifyApi
+    next()
+  }
+
   static homepage(_, res) {
     return res.render('homepage')
   }
@@ -14,6 +44,17 @@ class PublicRouter {
     res.render('uploadphoto')
   }
 
+  static async playlistSelection(req, res, next) {
+    try {
+      const data = await req.spotifyApi.getUserPlaylists({ limit: 50 })
+      console.log(data.body.items)
+      res.render('playlistselection', { playlists: data.body.items })
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
+  }
+
   static result(_, res) {
     res.render('result')
   }
@@ -23,6 +64,7 @@ class PublicRouter {
 router.get('/', PublicRouter.homepage)
 router.get('/getstarted', PublicRouter.getStarted)
 router.get('/uploadphoto', PublicRouter.uploadPhoto)
+router.get('/playlistselection', PublicRouter.middleware, PublicRouter.playlistSelection)
 router.get('/result', PublicRouter.result)
 
 module.exports = router
