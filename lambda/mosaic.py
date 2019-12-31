dcb7555c5539f1118da90e37abf8f167f04d77f5
Ram Goli <ramsgoli@gmail.com>
2019-12-30 20:06:52 -0800

    finalize the ALGORITHMS

--- lambda/mosaic.py
+++ lambda/mosaic.py
@@ -6,7 +6,7 @@ from multiprocessing import Process, Queue, cpu_count
 # Change these 3 config parameters to suit your needs...
 TILE_SIZE      = 100        # height/width of mosaic tiles in pixels
 TILE_MATCH_RES = 5        # tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT    = 8        # the mosaic image will be this many times wider and taller than the original
+ENLARGEMENT    = 3        # the mosaic image will be this many times wider and taller than the original
 
 TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
