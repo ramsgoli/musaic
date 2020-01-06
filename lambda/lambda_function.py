f406b612ae1e0fe28740cf5d5264fbdb7b0e8a23
Ram Goli <ramsgoli@gmail.com>
2020-01-06 14:48:12 -0800

    can get base64 encoding of output image"

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,7 +3,7 @@ import spotipy
 import tempfile
 from urllib import urlretrieve
 import multiprocessing
-from os import listdir, path, rename
+from os import listdir, path, rename, path
 import shutil
 from mosaic import mosaic
 import base64
@@ -28,7 +28,7 @@ def get_playlist_ids(sp, playlist_id):
 
     return ids
 
-def download_album_cover_art(sp, playlist_ids, image_path, temp_dir):
+def download_album_cover_art(sp, playlist_ids, temp_dir):
 
     processes = []
     playlist_ids = list(playlist_ids)
@@ -49,9 +49,6 @@ def download_album_cover_art(sp, playlist_ids, image_path, temp_dir):
 
     print "Finished downloading all files"
 
-    # generate mosaic
-    mosaic(image_path, temp_dir)
-
 
 def write_photo_data_to_file(file_data, temp_dir):
     file_path = path.join(temp_dir, "input_image.jpeg")
@@ -62,44 +59,55 @@ def write_photo_data_to_file(file_data, temp_dir):
     return file_path
 
 def get_image_from_s3(image_key):
+    INPUT_IMAGE = 'input_image.jpg'
+
     client = boto3.resource('s3')
     bucket = client.Bucket('musaic')
-    with open('input_image.jpg', 'wb') as f:
-        bucket.download_fileobj('photo.jpg', f)
+    with open(INPUT_IMAGE, 'wb') as f:
+        bucket.download_fileobj(image_key, f)
 
+    return INPUT_IMAGE
 
-    
-    
 def lambda_handler(event, context):
     playlist_id = event["playlist_id"]
-    image_data = event["image_data"]
+    file_name = event["file_name"]
     access_token = event["access_token"]
 
     sp = spotipy.Spotify(auth=access_token)
     
-    # get all unique album ideas 
+    # get all unique album ids 
     playlist_ids = get_playlist_ids(sp, playlist_id)
 
-    # download all album cover for this playlist
+    # download album cover
     temp_dir = tempfile.mkdtemp()
+    download_album_cover_art(sp, playlist_ids, temp_dir)
 
-    # write image_data to a file and get file path
-    image_path = write_photo_data_to_file(image_data, temp_dir)
-    download_album_cover_art(sp, playlist_ids, image_path, temp_dir)
+    # fetch image stored in s3
+    image_path = get_image_from_s3(file_name)
 
+    # generate mosaic
+    mosaic(image_path, temp_dir)
+
+    # Convert image into utf-8 encoded base64
     with open(path.join(temp_dir, "mosaic.jpeg"), "rb") as imageFile:
-	str_data = base64.b64encode(imageFile.read())
-	encoded_img = str_data.decode("utf-8")
+      str = base64.b64encode(imageFile.read())
+      encoded_img = str.decode("utf-8")
 
+    print(encoded_img)
     # clean up
-    shutil.rmtree(temp_dir)
+    #shutil.rmtree(temp_dir)
 
     return {
 	"isBase64Encoded": True,
 	"statusCode": 200,
 	"headers": { "content-type": "image/jpeg"},
-	"body":  encoded_img
     }
 
 if __name__ == '__main__':
-    get_image_from_s3('test')
+    event = {
+        "playlist_id": "1tzytA4QOHs4HYfIcOGsNV",
+        "file_name": "photo.jpg",
+        "access_token": "BQDjVaNEhOhqHHWAvLUcz81RGgDJBBIy85iCD5A8WBQ173UP_OS2Shr1H-3VEpldzvorx6-5TUUAs6ZUisrfQo4sWvVd2zeRdYq1SPt-CHVkpzOWR1cgb0l7NcKeaUh8TRJAEdqVW9bxTCalXmuvOcK9vcQJxXN9xAES7tNOh_kPYM9QVDhALA"
+    }
+    context = "context"
+    lambda_handler(event, context)
