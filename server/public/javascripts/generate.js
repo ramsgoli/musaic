3feda330e4ee0d4b115b7a070d16e93cf3a1f3fc
Ram Goli <ramsgoli@gmail.com>
2020-01-27 09:57:36 -0800

    get access token for api request from document cookie

--- server/public/javascripts/generate.js
+++ server/public/javascripts/generate.js
@@ -1,16 +1,25 @@
+const getCookie = name => {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
+
 document.getElementById("generate_photo_form").addEventListener('submit', e => {
   e.preventDefault()
   
-  // get fileName from session storage
+  // get fileName  from session storage
   const fileName = sessionStorage.getItem("fileName")
 
   // get playlist id from url
   const splitUrl = window.location.href.split("/")
   const playlistId = splitUrl[splitUrl.length - 1]
 
+  // get access token from cookies
+  const accessToken = getCookie("access_token")
+
   const body = {
-    playlistId,
-    fileName
+    playlist_id: playlist,
+    file_name: fileName,
+    access_token: accessToken
   }
 
   // call api with access token
