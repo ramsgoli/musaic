851c91878ff729e97f5475bfecf812d1d0ac2ebb
Ram Goli <ramsgoli@gmail.com>
2020-04-09 22:03:35 -0700

    make download button on results page functional

--- server/public/javascripts/loadResult.js
+++ server/public/javascripts/loadResult.js
@@ -3,3 +3,7 @@ const img = document.createElement('img')
 img.src = urlParams.get('key')
 const resultDiv = document.getElementById('result')
 resultDiv.appendChild(img)
+
+document.getElementById('download').addEventListener('click', () => {
+  window.location.href=urlParams.get('key')
+})
